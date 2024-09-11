aa834766584b058d41040b01720762a3f7fe37e0
Al-Am123 <aluno_manha1234@hotmail.com>
2024-09-11 10:22:47 -0300

    add filmes: gênero (misterio)

--- main.py
+++ main.py
@@ -141,7 +141,16 @@ def main():
                   09 > Oldboy (2003)
                   10 > Prisioners (2013)''')
         elif escolha == 10:
-            pass
+            print("""Se7en (1995) - Dirigido por David Fincher
+Gone Girl (2014) - Dirigido por David Fincher
+The Girl with the Dragon Tattoo (2011) - Dirigido por David Fincher
+The Sixth Sense (1999) - Dirigido por M. Night Shyamalan
+Zodiac (2007) - Dirigido por David Fincher
+Memories of Murder (2003) - Dirigido por Bong Joon-ho
+The Chaser (2008) - Dirigido por Na Hong-jin
+The Handmaiden (2016) - Dirigido por Park Chan-wook
+Oldboy (2003) - Dirigido por Park Chan-wook
+The Drug King (2018) - Dirigido por Woo Min-h""")
         elif escolha == 11:
             pass
         elif escolha == 12:
